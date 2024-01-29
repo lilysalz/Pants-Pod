64eefd99c0f386907a6fb800eccffd7d65ffeb2d
Omari <omarijthomas@hotmail.com>
2024-01-28 20:56:36 -0500

    merged

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -1,34 +1,11 @@
 //@ts-check
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
 import App from './App.jsx'
-import Home from './Home.jsx'
-import EpisodeList from './Episodes/EpisodeList'
 import './index.css'
 
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: 'episodes/',
-                element: <EpisodeList />
-            }
-        ]
-    }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
+        <App />
     </React.StrictMode>
 )
